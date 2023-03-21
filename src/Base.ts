566ca3de8380664ab27aa338051a55b31172743b
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-03-21 21:38:38 +0900

    :zap: APIのBaseUrlの記述位置を移動

--- src/Base.ts
+++ src/Base.ts
@@ -1,10 +1,8 @@
 import { RequestClient } from '@suzuki3jp/utils';
 
-import { APIEndPoints } from './api-types/index';
-
 export class Base {
     public req: RequestClient;
     constructor() {
-        this.req = new RequestClient({ baseUrl: APIEndPoints.base });
+        this.req = new RequestClient({ baseUrl: 'https://api.nightbot.tv' });
     }
 }
